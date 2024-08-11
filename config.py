cf87793898d9ef2f43ec2356079ab7abdc263933
RolexPc <157006183+RolexPc@users.noreply.github.com>
2024-08-11 13:05:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ class Config:
     API_HASH = os.environ.get("API_HASH", "49aacd0bc2f8924add29fb02e20c8a16")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "6479594186:AAF9T3Omz2N-xeDNAAKP42MZMhGVp4Y_Huw") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
-    DB_URL = os.environ.get("DB_URL", "mongodb+srv://Fahadh:Fahadh@cluster0.cpeyano.mongodb.net/")
-    DB_NAME = os.environ.get("DB_NAME", "Fahadh")
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://Ramanan:Ramanan@cluster0.lvbe2wu.mongodb.net/")
+    DB_NAME = os.environ.get("DB_NAME", "Ramanan")
     PORT = int(os.environ.get("PORT", "8080"))
     OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '1297128957').split()]
 
