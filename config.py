56f9536e13311b258baccedeb8586fe3bb470731
RolexPc <157006183+RolexPc@users.noreply.github.com>
2024-08-15 16:10:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from os import environ
 class Config:
     API_ID = os.environ.get("API_ID", "12618934")
     API_HASH = os.environ.get("API_HASH", "49aacd0bc2f8924add29fb02e20c8a16")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6518956653:AAF-j8WlImz7Y3bGuukewYORslS__wDvQAk") 
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7408432929:AAHWE6TQ6xpxb08B5-YoUW2l-TR98HsU-8g") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
     DB_URL = os.environ.get("DB_URL", "mongodb+srv://anus01:anus01@cluster0.dy9fh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DB_NAME = os.environ.get("DB_NAME", "anus01")
