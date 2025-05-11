5160c9687a4fe4338d22c50b968362c57c606331
𝗗𝗿 𝗕𝗲𝗻𝗷𝗮𝗺𝗶𝗻 𝗟𝗼𝘂𝗶𝘀 <157006183+rolexpc@users.noreply.github.com>
2025-05-11 12:51:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ from os import environ
 class Config:
     API_ID = os.environ.get("API_ID", "12618934")
     API_HASH = os.environ.get("API_HASH", "49aacd0bc2f8924add29fb02e20c8a16")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7408432929:AAHWE6TQ6xpxb08B5-YoUW2l-TR98HsU-8g") 
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
-    DB_URL = os.environ.get("DB_URL", "mongodb+srv://anus01:anus01@cluster0.dy9fh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    DB_NAME = os.environ.get("DB_NAME", "anus01")
+    DB_URL = os.environ.get("DB_URL", "")
+    DB_NAME = os.environ.get("DB_NAME", "")
     PORT = int(os.environ.get("PORT", "8080"))
     OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '7425490417').split()]
 
